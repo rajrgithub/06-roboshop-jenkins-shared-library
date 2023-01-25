2ae34e721b5e8023e47897e4415b48093dfb5c11
brajasekharreddy <raj479.mits@gmail.com>
2023-01-26 00:20:27 +0530

    groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,4 +1,18 @@
 def testgroovyfun() {
+    print 'function name should be mentioned'
+    print 'test.testgroovyfun()'
+    def abc = "test"
+    def xyz = 79
+
+    print "abc= ${abc}"
+    print "xyz=${xyz}"
+
+    print abc
+}
+
+def call() {
+    print 'Call is default function called by default even if function name is NOT mentioned'
+    print 'test()'
     def abc = "hello"
     def xyz = 10
 
