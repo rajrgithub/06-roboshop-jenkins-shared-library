4b2ac556ffeda04954eb6475884a7a5e13d70769
brajasekharreddy <raj479.mits@gmail.com>
2023-01-29 22:34:27 +0530

    Unit Test cases

--- vars/common.groovy
+++ vars/common.groovy
@@ -6,3 +6,20 @@ def compile() {
         sh 'mvn package'
     }
 }
+
+def unittests() {
+
+    if (app_lang == "nodejs") {
+        // Developer is missing unit test cases in our project, He need to add them as best practice, We are skipping to proceed further
+        sh 'npm test'
+
+    }
+
+    if (app_lang == "maven") {
+        sh 'mvn test'
+    }
+
+    if (app_lang == "python") {
+        sh 'python3 -m unittest'
+    }
+}
