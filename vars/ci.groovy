45f4cec486623191f22a1a9f69645588d4d954de
brajasekharreddy <raj479.mits@gmail.com>
2023-01-30 23:06:11 +0530

    Sonar Qube integration

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -25,8 +25,13 @@ def call() {
                 }
 
                 stage('Quality Control') {
+                    environment {
+                        SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                    }
                     steps {
                         echo 'Quality Control'
+                        sh "sonar-scanner -Dsonar.host.url=http://172.31.10.140:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
                     }
                 }
 
