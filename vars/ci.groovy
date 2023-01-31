0c5a3bebe4a8c439a374cd9ea8bbf1a98fbb15f8
brajasekharreddy <raj479.mits@gmail.com>
2023-01-31 21:49:34 +0530

    to skip unit tests for nodejs

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -26,11 +26,12 @@ def call() {
 
                 stage('Quality Control') {
                     environment {
-                        SONAR_USER = aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'
-                        SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        //SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
                     }
                     steps {
                       script {
+                          SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
                           echo 'Quality Control'
                           println "Password= ${SONAR_PASS}"
                           sh "echo sh Password= ${SONAR_PASS}"
