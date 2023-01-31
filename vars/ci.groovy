548e139e3536abbed80ab8b15cade9a52ce42fe5
brajasekharreddy <raj479.mits@gmail.com>
2023-01-31 21:41:15 +0530

    to skip unit tests for nodejs

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -30,10 +30,12 @@ def call() {
                         SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
                     }
                     steps {
-                        echo 'Quality Control'
-                        println "Password= ${SONAR_PASS}"
-                        sh "echo sh Password= ${SONAR_PASS}"
-                        sh "sonar-scanner -Dsonar.host.url=http://172.31.10.140:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+                      script {
+                          echo 'Quality Control'
+                          println "Password= ${SONAR_PASS}"
+                          sh "echo sh Password= ${SONAR_PASS}"
+                          sh "sonar-scanner -Dsonar.host.url=http://172.31.10.140:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
+                      }
                     }
                 }
 
