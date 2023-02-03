307c871ee064e3d688b511f4d6ea6c18973f4f4c
brajasekharreddy <raj479.mits@gmail.com>
2023-02-03 23:59:15 +0530

    NodeJS Artifact creation and Upload ZIP

--- vars/common.groovy
+++ vars/common.groovy
@@ -42,7 +42,7 @@ def artifactPush() {
     NEXUS_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
     NEXUS_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
     wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEXUS_PASS}", var: 'SECRET']]]) {
-        sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.1.57:8081/repository/${component}/${component}-${TAG_NAME}.zip"
+        sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.10.74:8081/repository/${component}/${component}-${TAG_NAME}.zip"
     }
 
 }
