0305421c676cfcf8ec6ee9960b955ac1c0b59200
brajasekharreddy <raj479.mits@gmail.com>
2023-02-13 02:28:44 +0530

    Terraform Pipeline

---
+++ vars/terraform.groovy
@@ -0,0 +1,26 @@
+def call() {
+  pipeline {
+
+    agent {
+      node {
+        label 'workstation'
+      }
+    }
+
+    parameters {
+      string(name: 'INFRA_ENV', defaultValue: '', description: 'Enter Env like dev or prod')
+    }
+
+    stages {
+
+      stage('Terraform Init') {
+        steps {
+          sh "terraform init -backend-config=env-${INFRA_ENV}/state.tfvars"
+        }
+      }
+
+    }
+
+
+    }
+}
