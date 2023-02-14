1e5e9988761c0535894063b4f566f4fd6d8b98c7
brajasekharreddy <raj479.mits@gmail.com>
2023-02-14 23:27:17 +0530

    apply or destroy action

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -13,6 +13,7 @@ def call() {
 
     parameters {
       string(name: 'INFRA_ENV', defaultValue: '', description: 'Enter Env like dev or prod')
+      choice(name: 'ACTION', choices: ['apply' , 'destroy'], description: 'Action')
     }
 
     stages {
@@ -24,7 +25,7 @@ def call() {
       }
       stage('Terraform Apply') {
         steps {
-          sh "terraform apply -auto-approve -var-file=env-${INFRA_ENV}/main.tfvars"
+          sh "terraform ${ACTION} -auto-approve -var-file=env-${INFRA_ENV}/main.tfvars"
         }
       }
 
